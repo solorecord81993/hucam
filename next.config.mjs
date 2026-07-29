0362bd0138213b39cc154f2ad1d535d2d3277ced
solorecord81993 <solorecord81993@gmail.com>
2026-07-29 13:07:58 +0700

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/sw.js",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=0, must-revalidate",
+        },
+        {
+          key: "Service-Worker-Allowed",
+          value: "/",
+        },
+      ],
+    },
+  ],
+};
+
+export default nextConfig;
