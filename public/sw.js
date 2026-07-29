2de9a6f9c9f67efca985b69bf535fbaa6a2c27db
solorecord81993 <solorecord81993@gmail.com>
2026-07-29 22:27:13 +0700

    Refresh person silhouette PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hucam-person-gate-v5";
+const CACHE_NAME = "hucam-person-mask-v6";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg", "/icon-maskable.svg"];
 
 self.addEventListener("install", (event) => {
