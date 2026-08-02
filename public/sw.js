9173bc0a42084d663464c9fc75ee219b41487f5d
solorecord81993 <solorecord81993@gmail.com>
2026-08-02 17:15:55 +0700

    Refresh multi-color PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hucam-multi-person-v8";
+const CACHE_NAME = "hucam-multi-color-v9";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg", "/icon-maskable.svg"];
 
 self.addEventListener("install", (event) => {
